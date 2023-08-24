b03737ecf04c8dbdd7f78eace791be6f4585b460
lunasimp <133180054+lunasimp@users.noreply.github.com>
2023-08-24 22:54:30 +0700

    Add files via upload

---
+++ Shoe_Store/Shoe_Store/Storage.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Project
+{
+    partial class Storage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoEllipsis = true;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(201, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(273, 35);
+            label1.TabIndex = 0;
+            label1.Text = "Inventory Management";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(8, 150);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(663, 297);
+            dataGridView1.TabIndex = 1;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(103, 72);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 20);
+            label2.TabIndex = 2;
+            label2.Text = "ProductName";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(317, 72);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 20);
+            label3.TabIndex = 3;
+            label3.Text = "InsStock";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(430, 72);
+            label4.Name = "label4";
+            label4.Size = new Size(99, 20);
+            label4.TabIndex = 4;
+            label4.Text = "Quantity_Sale";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(546, 72);
+            label5.Name = "label5";
+            label5.Size = new Size(116, 20);
+            label5.TabIndex = 5;
+            label5.Text = "RemainingStock";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(8, 103);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(270, 27);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(284, 103);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(415, 103);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(546, 103);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 9;
+            // 
+            // Storage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Name = "Storage";
+            Size = new Size(685, 450);
+            Load += Storage_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+    }
+}
---
+++ Shoe_Store/Shoe_Store/Storage.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Project
+{
+    public partial class Storage : UserControl
+    {
+        private string connectionString = "server =LAPTOP-KUS4KDEA; database =Store;uid=sa;pwd=123;TrustServerCertificate=true;Integrated Security = true";
+
+        public Storage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Storage_Load(object sender, EventArgs e)
+        {
+            LoadStorageData();
+        }
+        private void LoadStorageData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = @"
+                        SELECT
+                            P.ProductName,
+                            P.InStock,
+                            COALESCE(SUM(C.Quantity), 0) AS Quantity_Sale,
+                            P.InStock - COALESCE(SUM(C.Quantity), 0) AS RemainingStock
+                        FROM Product AS P
+                        LEFT JOIN Customer AS C ON P.ProductId = C.ProductId
+                        GROUP BY P.ProductId, P.ProductName, P.InStock
+                        ORDER BY P.ProductId;
+                        ";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(command);
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        BindingSource bindingSource = new BindingSource();
+                        bindingSource.DataSource = dataTable;
+
+                        // Gán BindingSource vào DataGridView
+                        dataGridView1.DataSource = bindingSource;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi: " + ex.Message);
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+                // lay gia tri tu dataGirdView
+                int instock = Convert.ToInt32(selectedRow.Cells["InStock"].Value);
+                string productName = Convert.ToString(selectedRow.Cells["ProductName"].Value);
+                int quantitysale = Convert.ToInt32(selectedRow.Cells["Quantity_Sale"].Value);
+                int tonkho = Convert.ToInt32(selectedRow.Cells["RemainingStock"].Value);
+                
+
+                textBox1.Text = productName;
+                textBox2.Text = instock.ToString();
+                textBox3.Text = quantitysale.ToString();
+                textBox4.Text = tonkho.ToString();
+                
+            }
+        }
+    }
+}
