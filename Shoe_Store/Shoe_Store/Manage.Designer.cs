1f85cbf5198db01f1ace10f693f4f9445d9a98d4
lunasimp <133180054+lunasimp@users.noreply.github.com>
2023-08-24 22:53:55 +0700

    Add files via upload

---
+++ Shoe_Store/Shoe_Store/Manage.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Project
+{
+    partial class Manage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            ProviderId = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Control;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(130, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(464, 35);
+            label1.TabIndex = 0;
+            label1.Text = "Manage the import and sale of products";
+            label1.UseWaitCursor = true;
+            label1.Click += label1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(3, 177);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(895, 266);
+            dataGridView1.StandardTab = true;
+            dataGridView1.TabIndex = 1;
+            dataGridView1.UseWaitCursor = true;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // ProviderId
+            // 
+            ProviderId.AutoSize = true;
+            ProviderId.Location = new Point(13, 78);
+            ProviderId.Name = "ProviderId";
+            ProviderId.Size = new Size(77, 20);
+            ProviderId.TabIndex = 2;
+            ProviderId.Text = "ProviderId";
+            ProviderId.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(113, 78);
+            label3.Name = "label3";
+            label3.Size = new Size(104, 20);
+            label3.TabIndex = 3;
+            label3.Text = "ProviderName";
+            label3.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(232, 78);
+            label4.Name = "label4";
+            label4.Size = new Size(116, 20);
+            label4.TabIndex = 4;
+            label4.Text = "Quantity-Import";
+            label4.UseWaitCursor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(345, 78);
+            label5.Name = "label5";
+            label5.Size = new Size(124, 20);
+            label5.TabIndex = 5;
+            label5.Text = "ProductId-Import";
+            label5.UseWaitCursor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(475, 78);
+            label6.Name = "label6";
+            label6.Size = new Size(100, 20);
+            label6.TabIndex = 6;
+            label6.Text = "ProductName";
+            label6.UseWaitCursor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(592, 78);
+            label7.Name = "label7";
+            label7.Size = new Size(57, 20);
+            label7.TabIndex = 7;
+            label7.Text = "InStock";
+            label7.UseWaitCursor = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(667, 78);
+            label8.Name = "label8";
+            label8.Size = new Size(99, 20);
+            label8.TabIndex = 8;
+            label8.Text = "Quantity-Sale";
+            label8.UseWaitCursor = true;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(786, 78);
+            label9.Name = "label9";
+            label9.Size = new Size(112, 20);
+            label9.TabIndex = 9;
+            label9.Text = "CustomerName";
+            label9.UseWaitCursor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(13, 117);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(77, 27);
+            textBox1.TabIndex = 10;
+            textBox1.UseWaitCursor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(96, 118);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(147, 27);
+            textBox2.TabIndex = 11;
+            textBox2.UseWaitCursor = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(257, 118);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(53, 27);
+            textBox3.TabIndex = 12;
+            textBox3.UseWaitCursor = true;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(345, 117);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(33, 27);
+            textBox4.TabIndex = 13;
+            textBox4.UseWaitCursor = true;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(600, 118);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(49, 27);
+            textBox5.TabIndex = 14;
+            textBox5.UseWaitCursor = true;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(415, 118);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(179, 27);
+            textBox6.TabIndex = 15;
+            textBox6.UseWaitCursor = true;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(726, 118);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(172, 27);
+            textBox7.TabIndex = 16;
+            textBox7.UseWaitCursor = true;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(667, 118);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(53, 27);
+            textBox8.TabIndex = 17;
+            textBox8.UseWaitCursor = true;
+            // 
+            // Manage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(ProviderId);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Name = "Manage";
+            Size = new Size(901, 446);
+            UseWaitCursor = true;
+            Load += Manage_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Label ProviderId;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+    }
+}
---
+++ Shoe_Store/Shoe_Store/Manage.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Data.SqlClient;
+using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Manage : UserControl
+    {
+        private string connectionString = "server =LAPTOP-KUS4KDEA; database =Store;uid=sa;pwd=123;TrustServerCertificate=true;Integrated Security = true";
+
+        public Manage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Manage_Load(object sender, EventArgs e)
+        {
+            LoadManageData();
+        }
+        private void LoadManageData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = @"
+                        SELECT
+                            COALESCE(P.ProviderId, 0) AS ProviderId,
+                            COALESCE(P.ProviderName, '') AS ProviderName,
+                            COALESCE(P.Quantity, 0) AS Quantity_import,
+                            COALESCE(P.ProductId, 0) AS ProductId_import,
+                            COALESCE(PR.ProductName, '') AS ProductName,
+                            COALESCE(PR.InStock, 0) AS InStock,
+                            COALESCE(C.Quantity, 0) AS Quantity_sale,
+                            COALESCE(C.CustomerName, '') AS CustomerName
+                        FROM Provider AS P
+                        LEFT JOIN Product AS PR ON P.ProductId = PR.ProductId
+                        LEFT JOIN Customer AS C ON PR.ProductId = C.ProductId;
+                        ";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(command);
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        BindingSource bindingSource = new BindingSource();
+                        bindingSource.DataSource = dataTable;
+
+                        // Gán BindingSource vào DataGridView
+                        dataGridView1.DataSource = bindingSource;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi: " + ex.Message);
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+                // lay gia tri tu dataGirdView
+                int providertId = Convert.ToInt32(selectedRow.Cells["ProviderId"].Value);
+                string providerName = Convert.ToString(selectedRow.Cells["ProviderName"].Value);
+                int quantityimport = Convert.ToInt32(selectedRow.Cells["Quantity_import"].Value);
+                int productidimport = Convert.ToInt32(selectedRow.Cells["ProductId_import"].Value);
+                string productname = Convert.ToString(selectedRow.Cells["ProductName"].Value);
+                int instock = Convert.ToInt32(selectedRow.Cells["InStock"].Value);
+                int quantitysale = Convert.ToInt32(selectedRow.Cells["Quantity_sale"].Value);
+                string customername = Convert.ToString(selectedRow.Cells["CustomerName"].Value);
+
+                textBox1.Text = providertId.ToString();
+                textBox2.Text = providerName;
+                textBox3.Text = quantityimport.ToString();
+                textBox4.Text = productidimport.ToString();
+                textBox6.Text = productname;
+                textBox5.Text = instock.ToString();
+                textBox8.Text = quantitysale.ToString();
+                textBox7.Text = customername;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
